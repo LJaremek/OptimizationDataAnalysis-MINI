b0f47dbac08e0d7fb5717b21f1bc523fbeca745e
wpartycja <w512patrycja@gmail.com>
2024-06-07 00:28:01 +0200

    refactor of main function to run expreiments on mnist

--- mlp-mnist/train_mnist.py
+++ mlp-mnist/train_mnist.py
@@ -4,127 +4,38 @@ An example script for training a model for the MNIST dataset with BITorch.
 Modified from the `PyTorch MNIST Example <https://github.com/pytorch/examples/blob/main/mnist/main.py>`_,
 which was published under the `BSD 3-Clause License <https://github.com/pytorch/examples/blob/main/LICENSE>`_.
 """
-# fmt: off
-import argparse
+
+import os
+import time
+import pickle
+from math import floor
 from datetime import datetime
 
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 from torch.optim.lr_scheduler import StepLR
 
-import bitorch.layers as qnn
-from bitorch import RuntimeMode
-from datasets import MNIST
-from bitorch.layers import convert
-import bitorch_engine
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+from binary_mlp import BinarizedMLP
+from mlp import MLP
+from tools import train, test
+from recorder import plot_results, setup_logging
+from parser_args import parse_arguments
 
 
-bitorch_engine.initialize()
-
-
-class QuantizedMLP(nn.Module):
-    def __init__(self, num_hidden_units_1=256, num_hidden_units_2=128):
-        super().__init__()
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(784, num_hidden_units_1)
-        self.act1 = nn.PReLU()
-        self.bn1 = nn.BatchNorm1d(num_hidden_units_1)
-
-        self.fc2 = qnn.QLinear(num_hidden_units_1, num_hidden_units_2, bias=False)
-        self.act2 = nn.PReLU()
-        self.bn2 = nn.BatchNorm1d(num_hidden_units_2)
-
-        self.fc3 = nn.Linear(num_hidden_units_2, 10)
-
-    def forward(self, x):
-        x = self.flatten(x)
-
-        x = self.fc1(x)
-        x = self.act1(x)
-        x = self.bn1(x)
-
-        x = self.fc2(x)
-        x = self.act2(x)
-        x = self.bn2(x)
-
-        x = self.fc3(x)
-        output = F.log_softmax(x, dim=1)
-        return output
-
-
-def train(args, model, device, train_loader, optimizer, epoch):
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
-            if args.dry_run:
-                break
-
-
-def test(model, device, test_loader):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
-
-    test_loss /= len(test_loader.dataset)
-
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
-
-
-def main():
-    # Training settings
-    parser = argparse.ArgumentParser(description='Binary PyTorch MLP')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=14, metavar='N',
-                        help='number of epochs to train (default: 14)')
-    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
-                        help='learning rate (default: 1.0)')
-    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
-                        help='learning rate step gamma (default: 0.7)')
-    parser.add_argument('--optimizer', type=str, choices=["adam", "adamax", "adadelta"], default="adadelta",
-                        help='optimizer to use (default: adadelta)')
-    parser.add_argument('--no-cuda', action='store_true',
-                        help='disables CUDA training')
-    parser.add_argument('--dry-run', action='store_true',
-                        help='quickly check a single pass')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
-    parser.add_argument('--save-model', action='store_true',
-                        help='For Saving the current Model')
-    args = parser.parse_args()
+if __name__ == "__main__":
+    args = parse_arguments()
 
     use_cuda = not args.no_cuda and torch.cuda.is_available()
-
     torch.manual_seed(args.seed)
-
     device = torch.device("cuda:0" if use_cuda else "cpu")
 
     train_kwargs = {'batch_size': args.batch_size}
     test_kwargs = {'batch_size': args.test_batch_size}
+    
+    
     if use_cuda:
         cuda_kwargs = {'num_workers': 1,
                        'pin_memory': True,
@@ -132,12 +43,22 @@ def main():
         train_kwargs.update(cuda_kwargs)
         test_kwargs.update(cuda_kwargs)
 
-    train_dataset, test_dataset = MNIST.get_train_and_test('./mnist', download=True)
 
-    train_loader = torch.utils.data.DataLoader(train_dataset, **train_kwargs)
-    test_loader = torch.utils.data.DataLoader(test_dataset, **test_kwargs)
+    # Dataset configuration
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+    train_dataset = datasets.MNIST('./mnist', train=True, download=True, transform=transform)
+    test_dataset = datasets.MNIST('./mnist', train=False, download=True, transform=transform)
+
+    train_loader = DataLoader(train_dataset, **train_kwargs)
+    test_loader = DataLoader(test_dataset, **test_kwargs)
     
-    model = QuantizedMLP().to(device)
+    if args.model == 'bimlp':
+        model = BinarizedMLP().to(device)
+    else:
+        model = MLP().to(device)
     
     # Optimizer selection
     if args.optimizer == "adam":
@@ -148,17 +69,63 @@ def main():
         optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
 
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+
+    # Set up logging
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M")
+    log_filename = f"{args.model}_{args.optimizer}_{timestamp}.txt"
+    logger = setup_logging(log_filename)
+
+    train_losses, test_losses = [], []
+    train_accuracies, test_accuracies = [], []
+
+    start_time = time.time()
+
+    # Training loop
     for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(model, device, test_loader)
+        train_loss, train_accuracy = train(args, model, device, train_loader, optimizer, epoch, args.log_interval, logger)
+        test_loss, test_accuracy = test(model, device, test_loader, logger)
         scheduler.step()
+        
+        train_losses.append(train_loss)
+        test_losses.append(test_loss)
+        train_accuracies.append(train_accuracy)
+        test_accuracies.append(test_accuracy)
 
-    test(model, device, test_loader)
+    # Time measuring
+    end_time = time.time()
+    training_time = end_time - start_time
 
+    log_message = f"Total training time: {floor(training_time//60)} min {round(training_time%60, 2)} seconds"
+    logger.info(log_message)
+    print(log_message)
+    
+    # Save the model
     if args.save_model:
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M")
-        model_name = f"bi_mlp_{args.optimizer}_{timestamp}.pt"
-        torch.save(model.state_dict(), f"./checkpoints/{model_name}")
+        model_dir = './checkpoints'
+        os.makedirs(model_dir, exist_ok=True)
+        model_name = f"{args.model}_{args.optimizer}_{timestamp}.pt"
+        model_path = os.path.join(model_dir, model_name)
+        torch.save(model.state_dict(), model_path)
 
-if __name__ == '__main__':
-    main()
+    # Plot results
+    plot_dir = './plots'
+    os.makedirs(plot_dir, exist_ok=True)
+    plot_filename = f"{args.model}_{args.optimizer}_{timestamp}.png"
+    plot_path = os.path.join(plot_dir, plot_filename)
+    epochs = range(1, args.epochs + 1)
+    plot_results(epochs, test_losses, train_accuracies, test_accuracies, plot_path)
+
+
+    # Save Accuracies for later comparing plots
+    acc_dir = './accuracies'
+    os.makedirs(model_dir, exist_ok=True)
+    train_acc_filename = f'{args.model}_{args.optimizer}_{timestamp}_train.pickle'
+    test_acc_filename = f'{args.model}_{args.optimizer}_{timestamp}_test.pickle'
+    
+    plot_path = os.path.join(plot_dir, plot_filename)
+    
+    with open(os.path.join(acc_dir, train_acc_filename), 'wb') as handle:
+        pickle.dump(train_accuracies, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    with open(os.path.join(acc_dir, test_acc_filename), 'wb') as handle:
+        pickle.dump(test_accuracies, handle, protocol=pickle.HIGHEST_PROTOCOL)
